c7b0b15975658cf28531212b46ad83f9cdd2692c
tamires_camargo <tamires.camargo.soares@gmail.com>
2023-08-24 18:08:29 -0300

    segundo commit correcao jest

--- src/components/Header/application.test.tsx
+++ src/components/Header/application.test.tsx
@@ -1,14 +1,15 @@
 import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import Header from '.'
 
 describe('Header', () => {
   test('renders corretamente comentarios', () => {
     render(<Header itensNoCarrinho={[]} />)
+
+    const firstComment = screen.getByTestId('frase-um')
+    expect(firstComment).toBeInTheDocument()
+
+    const secondComment = screen.getByTestId('frase-dois')
+    expect(secondComment).toBeInTheDocument()
   })
 })
-
-const firstComment = screen.getByTestId('frase-um')
-expect(firstComment).toBeInTheDocument()
-
-const secondComment = screen.getByTestId('frase-dois')
-expect(secondComment).toBeInTheDocument()
